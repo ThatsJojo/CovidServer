1006887fcc30c96e6eaf9f80a5531a5ac95f3a65
Cleyton Silva <cleyton.eu123@gmail.com>
2021-09-16 12:08:12 -0300

    Habilitando configuração default escolhida pelo Usuário

--- src/covidserver/CovidServer.java
+++ src/covidserver/CovidServer.java
@@ -2,25 +2,35 @@ package covidserver;
 
 import controller.ConnectionController;
 import java.util.Scanner;
-import model.UDPReceiver;
 
 public class CovidServer {
 
     public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
-        System.out.print("Insira o IP para receber os pacotes dos dispositivos: ");
-        String UDPReceiverIP = "127.0.0.1";//sc1.nextLine();
-        System.out.print("Agora a porta: ");
-        int UDPReceiverPort = 8085;//sc2.nextInt();
-        System.out.print("Insira o IP para enviar alertas aos dispositivos: ");
-        String UDPSenderIP = "127.0.0.1";//sc1.nextLine();
-        System.out.print("Agora a porta: ");
-        int UDPSenderPort = 8080;//sc2.nextInt();
-        System.out.print("Insira o IP para conexão com o médico: ");
-        String TCPReceiverIP = "127.0.0.1";//sc1.nextLine();
-        System.out.print("Agora a porta: ");
-        int TCPReceiverPort = 8083;//sc2.nextInt();
+        String UDPReceiverIP = "127.0.0.1";
+        int UDPReceiverPort = 8085;
+        String UDPSenderIP = "127.0.0.1";
+        int UDPSenderPort = 8080;
+        String TCPReceiverIP = "127.0.0.1";
+        int TCPReceiverPort = 8083;
+
+        System.out.println("Deseja configurar os endereços de comunicação? (sim/não)");
+        boolean flag = sc1.nextLine().equals("sim");
+        if (flag) {
+            System.out.print("Insira o IP para receber os pacotes dos dispositivos: ");
+            UDPReceiverIP = sc1.nextLine();
+            System.out.print("Agora a porta: ");
+            UDPReceiverPort = sc2.nextInt();
+            System.out.print("Insira o IP para enviar alertas aos dispositivos: ");
+            UDPSenderIP = sc1.nextLine();
+            System.out.print("Agora a porta: ");
+            UDPSenderPort = sc2.nextInt();
+            System.out.print("Insira o IP para conexão com o médico: ");
+            TCPReceiverIP = sc1.nextLine();
+            System.out.print("Agora a porta: ");
+            TCPReceiverPort = sc2.nextInt();
+        }
         ConnectionController cc = new ConnectionController(UDPReceiverIP, UDPReceiverPort, UDPSenderIP, UDPSenderPort, TCPReceiverIP, TCPReceiverPort);
         cc.startConnections();
     }
